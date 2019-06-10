807e0d706fcfa276ff11b485a45d4ee9280ce550
zhang.hp <zhang.hp@belle.com.cn>
2019-06-10 16:04:20 +0800

    升级2.0.7版本，增加服务注册、发现

--- src/main/java/com/coffee/Application.java
+++ src/main/java/com/coffee/Application.java
@@ -2,9 +2,11 @@ package com.coffee;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
+@EnableDiscoveryClient
 @SpringBootApplication()
 public class Application extends SpringBootServletInitializer {
 
--- src/main/java/com/coffee/controller/DemoController.java
+++ src/main/java/com/coffee/controller/DemoController.java
@@ -1,12 +1,20 @@
 package com.coffee.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author zhang.hp
  * @date 2018/12/18.
  */
+@Slf4j
 @RestController
 @RequestMapping("demo")
 public class DemoController {
@@ -15,4 +23,18 @@ public class DemoController {
     String  hello(){
         return "hello world";
     }
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/discovery", method = RequestMethod.GET)
+    public String index() {
+        List<ServiceInstance> instances = client.getInstances("springboot-demo");
+        for (int i = 0; i < instances.size(); i++) {
+            log.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+        }
+        return "discovery client";
+    }
+
+
 }
